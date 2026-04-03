a50903f9cfedf1f78f1a1369b62aa54181d99644
Linus Tucker <172833324+MrStarfleetCommand@users.noreply.github.com>
2026-04-02 23:31:12 -0400

    Refactor to use dynamic radius input

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 (() => {
 	const form = document.querySelector('form');
-	const r = 50;
 	const limit = 2.9 * Math.PI;
 
 	form.onsubmit = event => {
 		event.preventDefault();
 
-		const number = document.querySelector('#number').value;
-		const step = Math.PI / number * 2;
+		const sides = document.querySelector('#sides').value;
+		const radius = document.querySelector('#radius').value;
+		const step = Math.PI / sides * 2;
 		const points = [];
 		const h2 = document.createElement('h2');
 		const div = document.createElement('div');
 		const pre = document.createElement('pre');
 
 		for (let angle = Math.PI / 2; angle < limit; angle += step){
-			const x = r * (Math.cos(angle) + 1);
-			const y = r * (Math.sin(angle) + 1);
+			const x = radius * (Math.cos(angle) + 1);
+			const y = radius * (Math.sin(angle) + 1);
 			points.push(x.toFixed(3), y.toFixed(3));
 		}
 
 		const svg = `<?xml version="1.0" encoding="utf-8"?>
-<svg xmlns="http://www.w3.org/2000/svg" width="${r * 2}" height="${r * 2}">
+<svg xmlns="http://www.w3.org/2000/svg" width="${radius * 2}" height="${radius * 2}">
 	<path d="M ${points.join(' ')} Z"/>
 </svg>`;
 
-		h2.innerHTML = `Sides: ${number}`;
+		h2.innerHTML = `Sides: ${sides}`;
 		div.innerHTML = svg;
 		pre.textContent = svg;
 
